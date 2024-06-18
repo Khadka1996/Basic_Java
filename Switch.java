3f9aac12107763fcbb1222f760f916b39a403345
Khadka1996 <mnzkhadka1996@gmail.com>
2024-06-18 15:46:25 +0545

    I created Switch in Java

---
+++ Switch.java
@@ -0,0 +1,42 @@
+import java.util.*;
+
+
+public class Switch {
+    public static void main(String[] args) {
+    System.out.println("Please Press the Button to switch");
+    Scanner sc = new Scanner(System.in);
+    int button = sc.nextInt();
+    System.out.println("Press the button to switch");
+   switch(button){
+    case 1:
+        System.out.println("Button 1 is pressed");
+        break;
+    case 2:
+        System.out.println("Button 2 is pressed");
+        break;
+    case 3:
+        System.out.println("Button 3 is pressed");
+        break;
+    case 4:
+        System.out.println("Button 4 is pressed");
+        break;
+    case 5:
+        System.out.println("Button 5 is pressed");
+        break;
+    case 6:
+        System.out.println("Button 6 is pressed");
+        break;
+    case 7:
+        System.out.println("Button 7 is pressed");
+        break;
+    case 8:
+        System.out.println("Button 8 is pressed");
+        break;
+    case 9:
+        System.out.println("Button 9 is pressed");
+        break;
+    case 10:
+   }
+
+
+}}
