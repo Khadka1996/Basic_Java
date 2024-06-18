e363351f2c76538d382669029ca8311de02a8725
Khadka1996 <mnzkhadka1996@gmail.com>
2024-06-18 17:21:12 +0545

    I created Loops in Java

--- Loops.java
+++ Loops.java
@@ -9,17 +9,40 @@ public class Loops{
      //for (int i=0; i<11; i++){
      //System.out.println(i);
      //  }
-
+//**************************************************//
     // using while loop
     // int i = 0;
     // while(i<11){
     // System.out.println(i);
     // i = i + 1;
-
+//**************************************************//
     //using do while loop
+//     int i=0;
+//     do{
+//     System.out.println(i);
+//     i = i + 1;
+//     } while (i <11); 
+//   }
 
 
-   }
-  }
+//**************************************************//
+// sum of natural numbers
+// Scanner sc = new Scanner(System.in);
+//  int n = sc.nextInt();
+//  int sum=0;
+//  for (int i=0; i<=n; i++){
+//     sum = sum + i;
+//  }
+// System.out.println(sum);
+//   }
 
+//**************************************************//
+// Print the table of a number input by the user
+
+Scanner scanner = new Scanner (System.in);
+int n= scanner.nextInt();
+for (int i=1; i<10; i++) {
+   System.out.println(n*i);
 }
+  }}
+
