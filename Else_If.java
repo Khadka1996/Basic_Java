f4681ce58eb1691702e60ac3cbe40b56bf9ca47b
Khadka1996 <mnzkhadka1996@gmail.com>
2024-06-18 13:42:04 +0545

    I created Else If class

---
+++ Else_If.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+public class Else_If {
+public static void main(String[] args){
+
+Scanner sc = new Scanner(System.in);
+System.out.println("Enter the number to check odd and even");
+int x = sc.nextInt();
+
+if(x % 2 == 0){
+    System.out.println("The number is even");
+}
+else{
+    System.out.println("The number is odd");
+}
+
+ }
+}
