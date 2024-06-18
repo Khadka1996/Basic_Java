143576c1db95ad87f900f19c5042bb7db67fb05e
Khadka1996 <mnzkhadka1996@gmail.com>
2024-06-18 16:21:20 +0545

    I created Switch in Java

---
+++ Loops.java
@@ -0,0 +1,25 @@
+//print from 1 to 10 using loops
+
+import java.util.*;
+
+public class Loops{
+  public static void main(String[] args){
+
+     //using for loop
+     //for (int i=0; i<11; i++){
+     //System.out.println(i);
+     //  }
+
+    // using while loop
+    // int i = 0;
+    // while(i<11){
+    // System.out.println(i);
+    // i = i + 1;
+
+    //using do while loop
+
+
+   }
+  }
+
+}
